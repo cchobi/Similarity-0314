b9819c8c6b9846cd63ee6d8097f6c876f8b1e371
joenggyun.yu <jeonggyun@gmail.com>
2024-07-04 08:46:10 +0900

    Length second

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,9 +1,41 @@
 #include "pch.h"
 #include "../Similar/similar.cpp"
+#include <vector>
 
 using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, TestLengthSame) {
+	Similar sim;
+	string str1 = "ASD";
+	string str2 = "DSA";
+
+	int result = sim.checkCharNum(str1, str2);
+	EXPECT_EQ(result, 60);
+}
+
+TEST(TestCaseName, TestLengthDifferent1) {
+	Similar sim;
+	string str1 = "A";
+	string str2 = "BB";
+
+	int result = sim.checkCharNum(str1, str2);
+	EXPECT_EQ(result, 0);
+}
+
+TEST(TestCaseName, TestLengthDifferent2) {
+	Similar sim;
+	string str1 = "AAABB";
+	string str2 = "BAA";
+
+	int result = sim.checkCharNum(str1, str2);
+	EXPECT_EQ(result, 19);
+}
+
+TEST(TestCaseName, TestLengthDifferent3) {
+	Similar sim;
+	string str1 = "AA";
+	string str2 = "AAE";
+
+	int result = sim.checkCharNum(str1, str2);
+	EXPECT_EQ(result, 30);
 }
--- Similar/similar.cpp
+++ Similar/similar.cpp
@@ -1,7 +1,26 @@
 #include <string>
+#include <vector>
 using namespace std;
 
 class Similar {
 public:
+	int checkCharNum(const string& str1, const string& str2)
+	{
+		int longLen = 0;
+		int shortLen = 0;
+		if (str1.length() >= str2.length()) {
+			longLen = (int)str1.length();
+			shortLen = (int)str2.length();
+		}
+		else
+		{
+			longLen = (int)str2.length();
+			shortLen = (int)str1.length();
+		}
 
+		if (longLen == shortLen) return 60;
+		if (longLen == (2 * shortLen)) return 0;
+
+		return ((2 - (double)longLen / shortLen)*60);
+	}
 };
